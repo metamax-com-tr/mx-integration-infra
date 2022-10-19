8fd2fea20fa33bc95ed13e10d0af5e44a736d952
Murat Odunc <murat.odunc@orema.com.tr>
2022-10-19 18:31:32 +0300

    fixes

--- terraform/init-infra.sh
+++ terraform/init-infra.sh
@@ -1,9 +1,3 @@
-PROJECT_ID="5"
-TF_STAGE="${TF_STAGE}"
-TF_USERNAME="${GITLAB_USER_NAME}"
-TF_PASSWORD="${CI_JOB_TOKEN}"
-TF_ADDRESS="https://gitlab.orema.com.tr/api/v4/projects/${PROJECT_ID}/terraform/state/${TF_STAGE}"
-
 terraform init -upgrade \
   -backend-config=address=${TF_ADDRESS} \
   -backend-config=lock_address=${TF_ADDRESS}/lock \
@@ -13,4 +7,3 @@ terraform init -upgrade \
   -backend-config=lock_method=POST \
   -backend-config=unlock_method=DELETE \
   -backend-config=retry_wait_min=5
-

c48bd2f92a3d99e2b28c012eba77095b88b75222
Murat Odunc <murat.odunc@orema.com.tr>
2022-12-15 14:29:52 +0300

    Task#2349

--- terraform/security.tf
+++ terraform/security.tf
@@ -43,33 +43,44 @@ resource "aws_security_group" "bank_statements" {
 
 }
 
+resource "aws_security_group" "accounting_integration_deposit_processor" {
+  name        = "accounting_integration_deposit_processor"
+  description = "Account integration security"
+  vpc_id      = aws_vpc.aws_vpc.id
 
-# resource "aws_security_group" "lambda" {
-#   name        = "lambda_vpc_security-${local.environments[terraform.workspace]}-${var.namespace}"
-#   description = "allow inbound access to rds from ecs cluster"
-#   vpc_id      = aws_vpc.aws_vpc.id
 
-#   ingress {
-#     description     = "tcp"
-#     from_port       = 0
-#     to_port         = 65535
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.lb.id]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 65535
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
 
-# }
+resource "aws_security_group" "accounting_integration_withdrawal_processor" {
+  name        = "accounting_integration_withdrawal_processor"
+  description = "Account integration security"
+  vpc_id      = aws_vpc.aws_vpc.id
+
+
+  egress {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 
 resource "aws_security_group" "memory_db_for_redis" {

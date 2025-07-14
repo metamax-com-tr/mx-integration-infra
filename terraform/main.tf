2463944c9725d464c5dfc9a52323ad3bb8bafe0c
Murat Ödünç <murat.odunc@orema.com.tr>
2025-07-14 14:45:15 +0300

    remove unused resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,15 +31,3 @@ provider "aws" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 data "aws_caller_identity" "current" {}
 
-# Matamax
-module "metamax" {
-  source                   = "./modules/metamax"
-  aws_region               = var.aws_region
-  cidr                     = var.cidr
-  namespace                = var.namespace
-  environment              = local.environments[terraform.workspace]
-  availability_zones       = local.availability_zones[terraform.workspace]
-  metamax_banckend_subnets = local.metamax_banckend_subnets[terraform.workspace]
-}
-
-

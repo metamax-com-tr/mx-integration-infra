15eff2a22c388c6baa84941888cf4aaadc543553
Murat Ödünç <murat.odunc@orema.com.tr>
2025-08-27 15:50:09 +0300

    Task#189

--- terraform/github_ci_roles.tf
+++ terraform/github_ci_roles.tf
@@ -32,57 +32,57 @@ EOF
   }
 }
 
-resource "aws_iam_policy" "github_deployment_role_for_accounting_integration" {
-  name        = "github_deployment_role_for_accounting_integration"
-  description = "This role uses AWS Lambdas to deploy on regarding AWS resources"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "VisualEditor1",
-      "Effect": "Allow",
-      "Action": [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:GetObjectVersion",
-          "s3:GetObjectVersionAttributes",
-          "s3:ListMultipartUploadParts",
-          "s3:GetObjectAttributes",
-          "s3:GetObjectTagging"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${local.lambda_artifact_bucket[terraform.workspace]}/metamax-integrations-accounting/*"
-      ]
-    },
-    {
-      "Sid": "VisualEditor2",
-      "Effect": "Allow",
-      "Action": [
-          "s3:ListBucket",
-          "s3:ListMultipartUploadParts"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${local.lambda_artifact_bucket[terraform.workspace]}"
-      ]
-    },
-    {
-			"Sid": "VisualEditor3",
-			"Effect": "Allow",
-			"Action": "lambda:UpdateFunctionCode",
-			"Resource": [
-				"${aws_lambda_function.accounting_integration_processor.arn}"
-			]
-		}
-  ]
-}
-EOF
-}
+# resource "aws_iam_policy" "github_deployment_role_for_accounting_integration" {
+#   name        = "github_deployment_role_for_accounting_integration"
+#   description = "This role uses AWS Lambdas to deploy on regarding AWS resources"
+#   policy      = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "VisualEditor1",
+#       "Effect": "Allow",
+#       "Action": [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:GetObjectVersion",
+#           "s3:GetObjectVersionAttributes",
+#           "s3:ListMultipartUploadParts",
+#           "s3:GetObjectAttributes",
+#           "s3:GetObjectTagging"
+#       ],
+#       "Resource": [
+#         "arn:aws:s3:::${local.lambda_artifact_bucket[terraform.workspace]}/metamax-integrations-accounting/*"
+#       ]
+#     },
+#     {
+#       "Sid": "VisualEditor2",
+#       "Effect": "Allow",
+#       "Action": [
+#           "s3:ListBucket",
+#           "s3:ListMultipartUploadParts"
+#       ],
+#       "Resource": [
+#         "arn:aws:s3:::${local.lambda_artifact_bucket[terraform.workspace]}"
+#       ]
+#     },
+#     {
+# 			"Sid": "VisualEditor3",
+# 			"Effect": "Allow",
+# 			"Action": "lambda:UpdateFunctionCode",
+# 			"Resource": [
+# 				"${aws_lambda_function.accounting_integration_processor.arn}"
+# 			]
+# 		}
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy_attachment" "github_deployment_role_for_accounting_integration" {
-  role       = aws_iam_role.github_deployment_role_for_accounting_integration.name
-  policy_arn = aws_iam_policy.github_deployment_role_for_accounting_integration.arn
-}
+# resource "aws_iam_role_policy_attachment" "github_deployment_role_for_accounting_integration" {
+#   role       = aws_iam_role.github_deployment_role_for_accounting_integration.name
+#   policy_arn = aws_iam_policy.github_deployment_role_for_accounting_integration.arn
+# }
 
 
 # For https://github.com/metamax-com-tr/mx-integration-bank-deposit-gateway
--- terraform/metamax_accounting_integration_common.tf
+++ terraform/metamax_accounting_integration_common.tf
@@ -11,29 +11,29 @@ resource "aws_sqs_queue" "accounting_integration_fails" {
   }
 }
 
-resource "aws_secretsmanager_secret" "accounting_integration_processor" {
-  name = "${local.environments[terraform.workspace]}_accounting_integration_processor"
+# resource "aws_secretsmanager_secret" "accounting_integration_processor" {
+#   name = "${local.environments[terraform.workspace]}_accounting_integration_processor"
 
-  tags = {
-    NameSpace   = "accounting-integration"
-    Environment = "${local.environments[terraform.workspace]}"
-  }
-}
+#   tags = {
+#     NameSpace   = "accounting-integration"
+#     Environment = "${local.environments[terraform.workspace]}"
+#   }
+# }
 
-resource "aws_secretsmanager_secret_version" "accounting_integration_processor" {
-  secret_id     = aws_secretsmanager_secret.accounting_integration_processor.id
-  secret_string = <<EOF
-{
-  "musteri": "10000000",
-  "firma": "3782",
-  "kullaniciAdi": "metamax",
-  "parola": "metamax"
-}
-EOF
-  lifecycle {
-    ignore_changes = [
-      secret_string
-    ]
-  }
-}
+# resource "aws_secretsmanager_secret_version" "accounting_integration_processor" {
+#   secret_id     = aws_secretsmanager_secret.accounting_integration_processor.id
+#   secret_string = <<EOF
+# {
+#   "musteri": "10000000",
+#   "firma": "3782",
+#   "kullaniciAdi": "metamax",
+#   "parola": "metamax"
+# }
+# EOF
+#   lifecycle {
+#     ignore_changes = [
+#       secret_string
+#     ]
+#   }
+# }
 
